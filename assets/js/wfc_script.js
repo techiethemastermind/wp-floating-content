b4292f165fdc088c5d63b195d32ca4fdefbc139a
techiethemastermind <techiethemastermind@gmail.com>
2023-01-09 07:42:00 +0200

    Version 1.2.2

--- assets/js/wfc_script.js
+++ assets/js/wfc_script.js
@@ -1,17 +1,14 @@
 jQuery(document).ready(function($){
 
-	$('#wfc_pages').select2()
+	$('#wfc_pages').select2();
 
 	// Check logined or not
-	if (localStorage.getItem('accessToken')) {
+	if (localStorage.getItem('accessToken') && localStorage.getItem('user')) {
 
 		let accountMenu = $(account_menu);
-
 		let user = JSON.parse(localStorage.getItem('user'));
 
-		if (user) {
-			accountMenu.find('span#imma-account-name').text(user.name)
-		}
+		accountMenu.find('span#imma-account-name').text(user.name);
 
 		if ($('.wfc-account').length > 0) {
 			$('.wfc-account').css('display', 'inline-flex');
@@ -36,12 +33,13 @@ jQuery(document).ready(function($){
 		e.stopPropagation();
 
 		if ($(this).attr('data-action') == 'login') {
-			let loginUrl = window.location.protocol + '//' + window.location.host + window.location.pathname + '#/authentication/login-unguarded'
+			let loginUrl = window.location.protocol + '//' + window.location.host + window.location.pathname + '/imma/#/authentication/login-unguarded'
 			window.location.href = loginUrl;
 		}
 
 		if ($(e.target).attr('id') && $(e.target).attr('id') === 'imma-account-logout') {
 			localStorage.removeItem('accessToken');
+			localStorage.removeItem('user');
 			$('.wfc-account').find('.imma-dropdown').hide('fast');
 			window.location.reload();
 		} else if ($(this).attr('data-action') == 'account') {
